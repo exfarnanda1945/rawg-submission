a599d42c57fc58fa00985d715b5b4d59757da636
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-11-18 00:14:41 +0700

    fix: remove action bar when in home fragment

--- app/src/main/java/com/exfarnanda1945/rawgsubmission/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/rawgsubmission/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.exfarnanda1945.rawgsubmission
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment ->
+                    supportActionBar?.hide()
+                else -> {
+                    supportActionBar?.show()
+                    supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#1a1c1e")))
+                }
+            }
+
+        }
         setupActionBarWithNavController(navController)
     }
 
